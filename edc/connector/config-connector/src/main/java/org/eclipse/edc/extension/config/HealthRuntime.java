e508863e10834ba753b7177689378001c4b796d9
JinsYin <jinsyin@gmail.com>
2025-04-28 10:54:37 +0800

    EDC Extension 添加 Runtime 入口运行和调试

---
+++ edc/connector/config-connector/src/main/java/org/eclipse/edc/extension/config/HealthRuntime.java
@@ -0,0 +1,9 @@
+package org.eclipse.edc.extension.config;
+
+import org.eclipse.edc.boot.system.runtime.BaseRuntime;
+
+/**
+ * Entry point
+ */
+public class HealthRuntime extends BaseRuntime {
+}
---
+++ edc/connector/health-connector/src/main/java/org/eclipse/edc/extension/health/HealthRuntime.java
@@ -0,0 +1,9 @@
+package org.eclipse.edc.extension.health;
+
+import org.eclipse.edc.boot.system.runtime.BaseRuntime;
+
+/**
+ * Entry point
+ */
+public class HealthRuntime extends BaseRuntime {
+}
